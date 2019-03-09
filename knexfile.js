abae0001453e5a38d856b9451ead6626e38e015e
cecil john tantay <cjbtantay@gmail.com>
2019-03-09 10:35:55 -0800

    Add knexfile and db boilerplate

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+// Update with your config settings.
+require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl = true;
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/pokedex.sqlite3'
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  }
+};
