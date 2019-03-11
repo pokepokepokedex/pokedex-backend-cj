97418701445d99a21e9734049e055247a5ab132e
cecil john tantay <cjbtantay@gmail.com>
2019-03-11 09:08:03 -0700

    Deploy to a different branch

--- api/pokemon/pokemonModel.js
+++ api/pokemon/pokemonModel.js
@@ -1,29 +1,12 @@
 const db = require('../../data/dbConfig');
 
+const getusers = () => {
+  return db('users');
+};
+
 const getAll = () => {
   console.log('hello');
-  return db('pokemon')
-    .select(
-      'pokedex_number as id',
-      'name',
-      'pokedex_number',
-      'type1',
-      'type2',
-      'classfication',
-      'height_m',
-      'weight_kg',
-      'abilities',
-      'base_happiness',
-      'hp',
-      'attack',
-      'defense',
-      'sp_attack',
-      'sp_defense',
-      'speed',
-      'generation',
-      'capture_rate'
-    )
-    .paginate(15, 1, true);
+  return db('pokemon');
 };
 
 const getById = id => {
@@ -54,5 +37,6 @@ const getById = id => {
 
 module.exports = {
   getAll,
-  getById
+  getById,
+  getusers
 };
