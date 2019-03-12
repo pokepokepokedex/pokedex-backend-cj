6b0480ef92784fae8588acc0172245ff46b6f880
cecil john tantay <cjbtantay@gmail.com>
2019-03-12 11:02:44 -0700

    change shape of getEverything route

--- api/pokemon/pokemonModel.js
+++ api/pokemon/pokemonModel.js
@@ -6,25 +6,7 @@ const getusers = () => {
 
 const getEverything = () => {
   console.log('hello');
-  return db('pokemon').select(
-    'pokedex_number as id',
-    'name',
-    'pokedex_number',
-    'type1',
-    'type2',
-    'height_m',
-    'weight_kg',
-    'abilities',
-    'base_happiness',
-    'hp',
-    'attack',
-    'defense',
-    'sp_attack',
-    'sp_defense',
-    'speed',
-    'generation',
-    'capture_rate'
-  );
+  return db('pokemon').select('pokedex_number as id', 'name');
 };
 
 const getAll = query => {
