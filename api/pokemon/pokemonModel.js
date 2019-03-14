de3d49b268b9943ffc41c45787d9c7fbd6353037
wisdomtooth <cbtsao47@gmail.com>
2019-03-13 22:18:48 -0500

    getEverything helper now returns both types

--- api/pokemon/pokemonModel.js
+++ api/pokemon/pokemonModel.js
@@ -1,60 +1,60 @@
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
 
 const getusers = () => {
-  return db('users');
+  return db("users");
 };
 
 const getEverything = () => {
-  console.log('hello');
-  return db('pokemon').select('pokedex_number as id', 'name');
+  console.log("hello");
+  return db("pokemon").select("pokedex_number as id", "name", "type1", "type2");
 };
 
 const getAll = query => {
-  console.log('hello');
+  console.log("hello");
   const { page = 1, limit = 8 } = query;
-  return db('pokemon')
+  return db("pokemon")
     .select(
-      'pokedex_number as id',
-      'name',
-      'pokedex_number',
-      'type1',
-      'type2',
-      'height_m',
-      'weight_kg',
-      'abilities',
-      'base_happiness',
-      'hp',
-      'attack',
-      'defense',
-      'sp_attack',
-      'sp_defense',
-      'speed',
-      'generation',
-      'capture_rate'
+      "pokedex_number as id",
+      "name",
+      "pokedex_number",
+      "type1",
+      "type2",
+      "height_m",
+      "weight_kg",
+      "abilities",
+      "base_happiness",
+      "hp",
+      "attack",
+      "defense",
+      "sp_attack",
+      "sp_defense",
+      "speed",
+      "generation",
+      "capture_rate"
     )
     .paginate(limit, page, true);
 };
 
 const getById = id => {
-  return db('pokemon')
+  return db("pokemon")
     .select(
-      'pokedex_number as id',
-      'name',
-      'pokedex_number',
-      'type1',
-      'type2',
-      'height_m',
-      'weight_kg',
-      'abilities',
-      'base_happiness',
-      'hp',
-      'attack',
-      'defense',
-      'sp_attack',
-      'sp_defense',
-      'speed',
-      'generation',
-      'capture_rate'
+      "pokedex_number as id",
+      "name",
+      "pokedex_number",
+      "type1",
+      "type2",
+      "height_m",
+      "weight_kg",
+      "abilities",
+      "base_happiness",
+      "hp",
+      "attack",
+      "defense",
+      "sp_attack",
+      "sp_defense",
+      "speed",
+      "generation",
+      "capture_rate"
     )
     .where({ pokedex_number: id })
     .first();
